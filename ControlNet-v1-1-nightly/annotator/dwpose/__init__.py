08bb6c96377190d94d1679fcbb7bd57193e34ee6
Chuck Lien <chuck.lien@gmail.com>
2024-05-31 15:58:26 +0800

    add function with face or not

--- ControlNet-v1-1-nightly/annotator/dwpose/__init__.py
+++ ControlNet-v1-1-nightly/annotator/dwpose/__init__.py
@@ -5,26 +5,30 @@
 # 4th Edited by ControlNet (added face and correct hands)
 
 import os
-os.environ["KMP_DUPLICATE_LIB_OK"]="TRUE"
 
-import torch
+os.environ["KMP_DUPLICATE_LIB_OK"] = "TRUE"
+
 import numpy as np
+import torch
+
 from . import util
 from .wholebody import Wholebody
 
-def draw_pose(pose, H, W):
-    bodies = pose['bodies']
-    faces = pose['faces']
-    hands = pose['hands']
-    candidate = bodies['candidate']
-    subset = bodies['subset']
+
+def draw_pose(pose, H, W, draw_face):
+    bodies = pose["bodies"]
+    faces = pose["faces"]
+    hands = pose["hands"]
+    candidate = bodies["candidate"]
+    subset = bodies["subset"]
     canvas = np.zeros(shape=(H, W, 3), dtype=np.uint8)
 
     canvas = util.draw_bodypose(canvas, candidate, subset)
 
     canvas = util.draw_handpose(canvas, hands)
 
-    canvas = util.draw_facepose(canvas, faces)
+    if draw_face == True:
+        canvas = util.draw_facepose(canvas, faces)
 
     return canvas
 
@@ -34,7 +38,7 @@ class DWposeDetector:
 
         self.pose_estimation = Wholebody()
 
-    def __call__(self, oriImg):
+    def __call__(self, oriImg, with_face):
         oriImg = oriImg.copy()
         H, W, C = oriImg.shape
         with torch.no_grad():
@@ -42,27 +46,27 @@ class DWposeDetector:
             nums, keys, locs = candidate.shape
             candidate[..., 0] /= float(W)
             candidate[..., 1] /= float(H)
-            body = candidate[:,:18].copy()
-            body = body.reshape(nums*18, locs)
-            score = subset[:,:18]
+            body = candidate[:, :18].copy()
+            body = body.reshape(nums * 18, locs)
+            score = subset[:, :18]
             for i in range(len(score)):
                 for j in range(len(score[i])):
                     if score[i][j] > 0.3:
-                        score[i][j] = int(18*i+j)
+                        score[i][j] = int(18 * i + j)
                     else:
                         score[i][j] = -1
 
-            un_visible = subset<0.3
+            un_visible = subset < 0.3
             candidate[un_visible] = -1
 
-            foot = candidate[:,18:24]
+            foot = candidate[:, 18:24]
 
-            faces = candidate[:,24:92]
+            faces = candidate[:, 24:92]
 
-            hands = candidate[:,92:113]
-            hands = np.vstack([hands, candidate[:,113:]])
-            
+            hands = candidate[:, 92:113]
+            hands = np.vstack([hands, candidate[:, 113:]])
+
             bodies = dict(candidate=body, subset=score)
             pose = dict(bodies=bodies, hands=hands, faces=faces)
 
-            return draw_pose(pose, H, W)
+            return draw_pose(pose, H, W, with_face)
--- ControlNet-v1-1-nightly/gradio_video2pose.py
+++ ControlNet-v1-1-nightly/gradio_video2pose.py
@@ -1,21 +1,24 @@
 import os
-import gradio as gr
 import sys
+
 import cv2
+import gradio as gr
 import numpy as np
 from moviepy.editor import VideoFileClip
 
-sys.path.append('.')
+sys.path.append(".")
 
-from annotator.dwpose import DWposeDetector
-from utils import get_fps, read_frames, save_videos_from_pil
-from annotator.util import resize_image, HWC3
 from PIL import Image
 
-os.environ['GRADIO_ALLOW_FLAGGING'] = "never"
+from annotator.dwpose import DWposeDetector
+from annotator.util import HWC3, resize_image
+from utils import get_fps, read_frames, save_videos_from_pil
+
+os.environ["GRADIO_ALLOW_FLAGGING"] = "never"
 detector = DWposeDetector()
 
-def video2pose(fps, source_video):
+
+def video2pose(fps, with_face, source_video):
     output_path = "./pose.mp4"
     orig_fps = get_fps(source_video)
     frames = read_frames(source_video)
@@ -25,19 +28,17 @@ def video2pose(fps, source_video):
         frame_pil = np.array(frame_pil, dtype=np.uint8)
         frame_pil = HWC3(frame_pil)
         frame_pil = resize_image(frame_pil, 512)
-        print(f'{i}/{total}')
-        result= detector(frame_pil)
+        print(f"{i}/{total}")
+        result = detector(frame_pil, with_face)
         result = HWC3(result)
         img = resize_image(frame_pil, 512)
         H, W, C = img.shape
-        result = cv2.resize(
-            result, (W, H), interpolation=cv2.INTER_LINEAR
-        )
+        result = cv2.resize(result, (W, H), interpolation=cv2.INTER_LINEAR)
         result = Image.fromarray(result)
         kps_results.append(result)
 
     save_videos_from_pil(kps_results, output_path, fps=orig_fps)
-    if (int(orig_fps) != fps):
+    if int(orig_fps) != fps:
         clip = VideoFileClip(output_path)
         new_clip = clip.set_fps(fps)
         new_output_path = f"./pose_fps_{fps}.mp4"
@@ -46,11 +47,16 @@ def video2pose(fps, source_video):
     print(get_fps(output_path), output_path)
     return os.path.join(os.getcwd(), output_path)
 
+
 app = gr.Interface(
     fn=video2pose,
-    inputs=[gr.Number(value=20, label="fps"), gr.Video()],
+    inputs=[
+        gr.Number(value=20, label="fps"),
+        gr.Checkbox(label="withFace", value=False),
+        gr.Video(),
+    ],
     outputs=[gr.Video()],
-    description="Tranform video to pose video"
+    description="Tranform video to pose video",
 ).queue()
 
-app.launch(server_name='0.0.0.0', share=True)
+app.launch(server_name="0.0.0.0", share=True)
